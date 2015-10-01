9de0b5a1cc13961f7d92b4150aff5ffef4a67024
Luis Sieira <luis.sieira@spacebar.fr>
2015-10-01 22:03:07 +0200

    add exclude dirs

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,10 +27,17 @@ module.exports = function(grunt) {
       test: {
         options: {
           quiet: false, // Optionally suppress output to standard out (defaults to false)
-          clearRequireCache: true, // Optionally clear the require cache before running tests (defaults to false)
-          exclude: 'node_modules'
+          clearRequireCache: true // Optionally clear the require cache before running tests (defaults to false)
         },
-        src: ['**/test/**/*.js']
+        src: ['**/test/**/*.js'],
+        filter: function(path) {
+          return [
+            'node_modules',
+            'bower_components'
+          ].every(function(regexp) {
+            return path.match(regexp) === null;
+          });
+        }
       }
     }
   });
